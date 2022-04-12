9939af644b27f62b0f2776f8556ac203bec4eb53
Rouh <roaming.new@gmail.com>
2022-04-13 00:12:23 +0900

    modify build files

--- generate-jre.bat
+++ generate-jre.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-.\jdk-17.0.1\bin\jdeps --list-deps --ignore-missing-deps .\build\libs\rouh-mahjong-net-1.0-SNAPSHOT-all.jar | findstr java > tmp.txt
+.\jdk-17\bin\jdeps --list-deps --ignore-missing-deps .\build\libs\rouh-mahjong-net-1.0-SNAPSHOT-all.jar | findstr java > tmp.txt
 for /f %%x in (tmp.txt) do (
     call set dep-list=%%dep-list%%,%%x
 )
@@ -9,7 +9,6 @@ set dep-list=%dep-list:~1%
 rem echo dep-list=%dep-list%
 
 del tmp.txt
-if exist ".\build\jre-min" rd /s /q ".\build\jre-min"
-rem if exist ".\jre-min" rd /s /q ".\jre-min"
+if exist ".\build\product\jre-min" rd /s /q ".\build\product\jre-min"
 
-.\jdk-17.0.1\bin\jlink --compress=2 --module-path "jdk-17.0.1/bin/jmods" --add-modules %dep-list% --output build\jre-min
+.\jdk-17\bin\jlink --compress=2 --module-path "jdk-17/bin/jmods" --add-modules %dep-list% --output build\product\jre-min
